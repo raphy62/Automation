198d44eebeed65bff2139d46e06789177e276cd4
raphy62 <101949429+raphy62@users.noreply.github.com>
2022-03-19 14:07:27 -0600

    Create TirageCode.py
    Tirage Code

---
+++ TirageCode.py
@@ -0,0 +1,17 @@
+from random import shuffle
+
+#COMPTENEUR
+lst = []
+
+# COMPTER DE 1 A 99
+for x in range(1,100):
+# GROUPER DANS LE COMPTENEUR    
+    lst.append(x)
+#REMUER
+    shuffle(lst)
+print("And the lucky numbers are")
+
+#TIRER JUSTE 6 NOMBRE DE 1 A 99
+print(*lst[0:6], sep="\n")
+print("You're the Winner!")
+print("Raphy GIT!")
